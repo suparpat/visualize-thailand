a14e4e8c07d28cf8ddfdb8dd60df5b2d60122deb
Pawat <pawat@mx-ll-110.164.164-161.static.3bb.co.th>
2017-07-29 15:14:21 +0700

    Add map

--- public/app.js
+++ public/app.js
@@ -1,1 +1,25 @@
-console.log('hi')
+var mymap = L.map('mapid').setView([13.5000, 100.9925], 5);
+
+
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mymap);
+
+
+
+
+
+
+$(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url: "data/[Thai] Coastal Water Quality 1998-2015 คุณภาพน้ำทะเลบริเวณชายฝั่งทะเล.csv",
+        dataType: "text",
+        success: function(data) {
+			var data = Papa.parse(data);
+			console.log(data)
+
+        }
+     });
+});
